6271e1ebbc132e2efba4a49ea4d03551c44fb0de
Yu-Yuan (Stuart) Yang <yu-yuan.yang@qmul.ac.uk>
2026-02-13 11:51:15 +0000

    Revise header comments and copyright details
    Updated file header with copyright and licensing information.

--- FragBEST_pymol_plugin/__init__.py
+++ FragBEST_pymol_plugin/__init__.py
@@ -1,6 +1,18 @@
 """
-Modified by Yu-Yuan Yang 2025
+This file is modified from masif_pymol_plugin.
+masif_pymol_plugin: https://github.com/LPDI-EPFL/masif/tree/master/source/masif_pymol_plugin
 
+Original work Copyright (c) 2019 Gainza P, Sverrisson F, Monti F, Rodola,
+Bronstein MM, Correia BE
+Modifications Copyright (c) 2024-2026 Yu-Yuan (Stuart) Yang /
+Arianna Fornili's Lab
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Original header:
 Pablo Gainza Cirauqui 2016 LPDI IBI STI EPFL
 This pymol plugin for Masif just enables the load ply functions.
 """
